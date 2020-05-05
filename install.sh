939ea90637a9722940f57d4e59d207e4df4e09c8
Cesar Medina <csmedina@gmail.com>
2020-05-05 12:06:23 -0300

    updating to work with cuda 10.2

--- install.sh
+++ install.sh
@@ -6,10 +6,17 @@ pyenv install 3.7.5
 pyenv virtualenv 3.7.5 flex
 pyenv activate flex
 
+#if problem with torch, try pip install torch===1.4.0 torchvision===0.5.0 -f https://download.pytorch.org/whl/torch_stable.html
+
 cd $LIBS_DIR
 git clone https://github.com/silvadirceu/segan_pytorch.git
 cd segan_pytorch
 pip install req.txt
+
+cd ..
+git clone https://github.com/santi-pdp/ahoproc_tools.git
+cd ahoproc_tools
+python setup.py install
 
 # download the model
 cd ckpt_segan+
